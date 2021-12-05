2161ad43e74e2b62203d54eccb3e142c5c28fb40
kortess <kortess1999@mail.ru>
2021-12-05 20:29:32 +0300

    v1.3 Reworked dataManager.py. Fixed __init__ for businessEntity.py.

--- businessEntity.py
+++ businessEntity.py
@@ -15,19 +15,39 @@ class BusinessEntity:
     regularOperationTypes = []
 
     def __init__(self):
+        # getting operation types
+        self.regularOperationTypes = []
+        all_types = DB.get_operation_types()
+        for op_type in all_types:
+            # we do not use op_type in RegularOperationType __init__
+            new_op_type = RegularOperationType(op_type["name"], const.REG_OP_STATUS_ACTIVE)
+            if not op_type["active"]:
+                new_op_type.delete()
+            self.regularOperationTypes.append(new_op_type)
+
         # getting active regular operations
         self.regularOperations = []
         active_operations = DB.get_active_regular_operations()
         for operation in active_operations:
-            # need to fix mapping for operation
-            self.regularOperations.append({"id": operation.id,
-                                           "operation": RegularOperation(operation.name, operation.reg_op_type, operation.payment_amount, operation.period, operation.notification_period, operation.start_date)})
+            # searching for RegularOperationType
+            for op_type in self.regularOperationTypes:
+                if op_type.name == operation["reg_op_type"]:
+                    # building dates and RegularOperation
+                    period = timedelta(days=float(operation["period"]))   # IMPORTANT FORMAT OF TIMEDELTA
+                    notification_period = timedelta(days=operation["notification_period"]) # IMPORTANT FORMAT OF TIMEDELTA
+                    start_date = date(
+                        year=int(operation["start_date"][:4]),
+                        month=int(operation["start_date"][5:7]),
+                        day=int(operation["start_date"][8:]))
+                    new_reg_op = RegularOperation(operation["name"], op_type, operation["payment_amount"], period, notification_period, start_date)
+                    self.regularOperations.append({"id": operation["id"], "operation": new_reg_op})
 
         # getting current balance on init
         cur_dep_balance = DB.get_deposit_balance()
         self.deposit_balance = DepositBalance(cur_dep_balance)
+        # **add here limits
 
-        # getting operation types
+
 
     def add_regular_operation(self, name, reg_op_type, payment_amount, period, notification_period, start_date):
         new_reg = RegularOperation(name, reg_op_type, payment_amount, period, notification_period, start_date)
@@ -38,26 +58,26 @@ class BusinessEntity:
 
     def change_regular_operation(self, operation_id, name, reg_op_type, payment_amount, period, notification_period):
         for operation in self.regularOperations:
-            if operation.id == operation_id:
-                operation.operation.update(name, reg_op_type, payment_amount, period, notification_period)
+            if operation["id"] == operation_id:
+                operation["operation"].update(name, reg_op_type, payment_amount, period, notification_period)
                 DB.change_regular_operation(operation)
                 return
 
     def remove_regular_operation(self, operation_id):
         for i in range(len(self.regularOperations)):
             operation = self.regularOperations[i]
-            if operation.id == operation_id:
-                operation.operation.delete()
+            if operation["id"] == operation_id:
+                operation["operation"].delete()
                 DB.remove_regular_operation(operation)
                 self.regularOperations.pop(i)
                 return
 
-    def change_balance(self, new_balance=0):
+    def change_deposit_balance(self, new_balance=0):
         if not isinstance(new_balance, int):
             raise TypeError("PaymentBalance: expected int for new_limit")
             return
         self.deposit_balance.balance.set_balance(new_balance)
-        DB.change_balance(new_balance)
+        DB.change_deposit_balance(new_balance)
 
     def form_statistics_by_period(self, tag, start_date, end_date):
         if not isinstance(tag, str):
